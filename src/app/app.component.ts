e1adc1eab438b31c3944352bea3b0f5194384fe9
Clenisson Calaça <clenisson@plus.com.vc>
2017-08-19 16:35:44 -0300

    Correção de duplicidade na importação AuthPage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,6 @@ import { Nav, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { AuthPage } from '../pages/auth/login';
 import { ProfilePage } from '../pages/profile/profile';
 import { DependentsPage } from '../pages/profile/dependents/dependents';
 import { DependentsListPage } from '../pages/profile/dependents/dependents-list';
