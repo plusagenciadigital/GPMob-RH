b460c956fdf3d0f0eee1bb2a73cb61b58b4f4244
Clenisson Calaça <clenisson@plus.com.vc>
2017-08-19 16:05:50 -0300

    Storage (início)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { ListPage } from '../pages/list/list';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { AuthServiceProvider } from '../providers/auth-service/auth-service';
+import { IonicStorageModule } from '@ionic/storage';
 
 @NgModule({
   declarations: [
@@ -27,6 +28,7 @@ import { AuthServiceProvider } from '../providers/auth-service/auth-service';
     BrowserModule,
     HttpModule,
     IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot()    
   ],
   bootstrap: [IonicApp],
   entryComponents: [
