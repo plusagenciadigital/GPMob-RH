0a2c2da339d782d77c7b7e1c79420710dd55a042
Danilo Rodrigues <dnrodriguesm@gmail.com>
2017-08-20 03:51:57 -0300

    Ajustando modal para reportar erro

--- src/pages/modals/report.ts
+++ src/pages/modals/report.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
-import { ViewController } from 'ionic-angular';
+import { ViewController, LoadingController, ToastController } from 'ionic-angular';
 
 @Component({
   selector: 'report',
   templateUrl: './report.html'
 })
 export class ReportError {
-  constructor(public viewCtrl: ViewController) {}
+  constructor(
+    public viewCtrl: ViewController,
+    public loadingCtrl: LoadingController,
+    public toastCtrl: ToastController
+  ) {}
+
+  request() {
+    let loader = this.loadingCtrl.create({
+      content: "Please wait..."
+    });
+    const toast = this.toastCtrl.create({
+      message: "Sua solicitação foi enviada com sucesso",
+      showCloseButton: true,
+      closeButtonText: 'Fechar'
+    });
+
+
+    loader.present();
+
+    setTimeout(() => {
+      loader.dismiss();
+    }, 1000)
+    setTimeout(() => {
+      this.viewCtrl.dismiss();
+      toast.present();
+    }, 1500);
+  }
 
   dismiss() {
     this.viewCtrl.dismiss();
